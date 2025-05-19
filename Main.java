a7483f3ed40d5feaa5f26bee879c6ce2bc7f02dc
Stephs <sajimenez@scl.edu.gt>
2025-05-19 03:11:31 -0600

    Creacion de Login

--- Main.java
+++ Main.java
@@ -1,22 +1,33 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
+
+import vistas.LoginPanel;
 
 public class Main {
 
     public static void main(String[] args) {
         
         //Crear una ventana con JFrame
-        JFrame miVentana = new JFrame();
-        miVentana.setTitle("Hospital SCL"); 
-        miVentana.setSize(1000, 700);
-        miVentana.setVisible(true);
+        JFrame miVentana = new JFrame();  //Se crea una ventana 
+        miVentana.setTitle("Hospital SCL");  //Se pone titulo a la ventana
+        miVentana.setSize(1000, 800); //Tamaño de la ventana (ancho, alto)
+        miVentana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        miVentana.setLayout(new BorderLayout());
 
-        //Crear un texto con JLabel
-        JLabel texto  = new JLabel();
-        texto.setText("Hola mundo");
-        miVentana.add(texto);
+        //Panel Banner
+        JPanel panelBanner = new JPanel();
+        panelBanner.setPreferredSize(new Dimension(500,500));
+        panelBanner.setBackground(Color .RED);
+        panelBanner.setOpaque(true);
+        miVentana.add(panelBanner,BorderLayout.WEST);
+
+        LoginPanel panelLogin = new LoginPanel();
+        miVentana.add(panelLogin.getJPanel(), BorderLayout.CENTER);
+
+        miVentana.setVisible(true); //Se muestra la ventana en la pantalla
     }
     
 }
---
+++ vistas/LoginPanel.java
@@ -0,0 +1,64 @@
+package vistas;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+public class LoginPanel {
+
+    private JPanel panelLogin;
+
+    public LoginPanel(){ 
+        panelLogin = new JPanel();
+        panelLogin.setSize(500, 500);
+        panelLogin.setOpaque(true);
+        panelLogin.setBackground(Color. PINK);
+        panelLogin.setLayout(new GridBagLayout());
+
+        //Admin Diseno
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 80, 10, 80);
+        gbc.fill = GridBagConstraints. HORIZONTAL;
+        gbc.weightx = 1.0;
+
+        //titulo
+        JLabel titulo = new JLabel("Login");
+        titulo.setOpaque(true);
+        titulo.setBackground(Color.BLUE);
+        gbc.gridy=0;
+        gbc.gridx=0;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        titulo.setHorizontalAlignment(SwingConstants.CENTER);
+        panelLogin.add(titulo, gbc);
+
+        //Usuario
+        JTextField entradaCorreo = new JTextField("Usuario: ");
+        entradaCorreo.setSize(new Dimension(100,50));
+        gbc.gridx = 1;
+        gbc.gridy =1;
+        panelLogin.add(entradaCorreo,gbc);
+
+        //Contraseña
+        JTextField entradaContraseña = new JTextField("Contraseña");
+        entradaContraseña.setSize(new Dimension(100,50));
+        gbc.gridx = 1;
+        gbc.gridy =2;
+        panelLogin.add(entradaContraseña,gbc);
+    
+        //Inicio
+        JButton btnIngresar = new JButton("Ingresar");
+        btnIngresar.setPreferredSize(new Dimension(100,50));
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.fill = GridBagConstraints.NONE;
+        panelLogin.add(btnIngresar,gbc);
+    }
+    public JPanel getJPanel(){
+        return panelLogin;
+    }
+}
